cc4ebf139ac7a9350e68ad8a2b15643c548dc911
Charles <649597734@qq.com>
2017-07-16 17:48:49 +0800

    day7 simple MVC

--- www/coroweb.py
+++ www/coroweb.py
@@ -40,7 +40,7 @@ def post(path):
 def get_required_kw_args(fn):
     args = []
     params = inspect.signature(fn).parameters
-    for name, param in params.items:
+    for name, param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
             args.append(name)
     return tuple(args)
